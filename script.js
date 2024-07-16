8b8e04ba057111d80ef45a55d28e94b3cd8f5f86
collins Erim <collinkesh061@gmail.com>
2024-07-17 00:47:06 +0100

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const hamburger = document.querySelector(".hamburger");
-const navlinks = document.querySelector(".nav-links");
+// const hamburger = document.querySelector(".hamburger");
+// const navlinks = document.querySelector(".nav-links");
 
-hamburger.onclick = function () {
-    hamburger.classList.toggle("active");
-    navlinks.classList.toggle("active");
+// hamburger.onclick = function () {
+//     hamburger.classList.toggle("active");
+//     navlinks.classList.toggle("active");
+// }
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = "flex"
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = "none"
 }
